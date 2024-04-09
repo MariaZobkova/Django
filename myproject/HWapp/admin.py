fb192341a129b25a366eb6eacec570754b155afc
Maria <mzobkova@bk.ru>
2024-04-09 21:07:18 +0300

    HW5 added

--- myproject/HWapp/admin.py
+++ myproject/HWapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'count']
+    ordering = ['price', '-count']
+    list_filter = ['price']
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+
+
